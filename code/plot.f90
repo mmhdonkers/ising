186ead1efffbe414c4b7f89e90f94399cd602a3a
Chiel Donkers <mmhdonkers@vvtp.tudelft.nl>
2013-03-20 23:37:55 +0100

    added module

---
+++ code/plot.f90
@@ -0,0 +1,54 @@
+module plot
+  implicit none
+  
+  public plot_init, plot_close, plot_spin
+
+contains
+
+  function plot_init
+    use plplot
+
+    call plsdev("xcairo")
+
+    call plscol0(0, 255, 255, 255)  ! white
+    call plscol0(1, 255, 0, 0)  ! red
+    call plscol0(2, 0, 255, 0)  ! green
+    call plscol0(3, 0, 0, 255)  ! blue
+    call plscol0(4, 255, 0, 255)  ! magenta
+    call plscol0(5, 0, 255, 255)  ! cyan
+    call plscol0(6, 255, 255, 0)  ! yellow
+    call plscol0(7, 0, 0, 0)  ! black
+    call plscol0(8, 255, 77, 0)  ! orange
+    call plscol0(9, 128, 128, 128)  ! gray
+
+    call plinit()
+  end function
+
+  function plot_close
+    call plspause(.false.)
+    call plend()
+  end function
+
+  function plot_spin(spin,size)
+    integer,intent(in) :: spin(:,:), size
+
+    integer :: i, j
+
+    call plcol0(7)
+    call plenv(0, size, 0, size, 0, 0)
+    call pllab("x", "y", "spin")
+
+    do i = 0, size - 1
+      do j = 0, size - 1
+        if (spin(i,j) .eq. -1) then
+          plcol0(1)
+          plpoin(i + 0.5d0, j + 0.5d0, 2)
+        else if (spin(i,j) .eq. 1) then
+          plcol0(2)
+          plpoin(i + 0.5d0, j + 0.5d0, 2)
+        end if
+      end do
+    end do
+  end function
+
+end module
