7457675de1e2a1e284f196565dfd021d63b30b9f
Chiel Donkers <mmhdonkers@vvtp.tudelft.nl>
2013-04-05 12:12:34 +0200

    added all parameter calculations (magnetization, Binder Cumulant, Cv, magnetic susceptibility, temp

--- code/ising.f90
+++ code/ising.f90
@@ -51,11 +51,11 @@ program ising
 
 !! fortran begins indexing from 1. Start it from 0 because the rand() starts from 0
   integer :: spin(0:SIZE-1,0:SIZE-1), temp, time
-  real(8) :: weight(-2:2), spintotal
-  integer :: wolffspin(0:SIZE-1, 0:SIZE-1)
-  real(8) :: wolfftotal
-  integer :: swenwangspin(0:SIZE-1, 0:SIZE-1)
-  real(8) :: swenwangtotal
+  real(8) :: weight(-2:2), spintotal, sqspin, qspin, etot, sqetot, absspin
+  integer :: wolffspin(0:SIZE-1, 0:SIZE-1), Nswc
+  real(8) :: wolfftotal, sqwol, qwol, ewol, sqewol, magsupwol
+  integer :: swenwangspin(0:SIZE-1, 0:SIZE-1), Nsum
+  real(8) :: swenwangtotal, sqsw, qsw, esw, sqesw, magsupsw
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Main Body !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -79,42 +79,87 @@ program ising
 ! Re-initialize Wolff lattice. All values in the lattice must be 1
     wolffspin(:,:) = 1
     wolfftotal = 0
+    sqwol = 0
+    qwol = 0
+    ewol = 0
+    sqewol = 0
+    magsupwol = 0
     do time = 1, TIMEFINAL, 100
-      call wolff(wolffspin, SIZE, temp/100d0)
+      call wolff(wolffspin, SIZE, temp/100d0, Nswc)
       wolfftotal = wolfftotal + abs(sum(wolffspin)/(SIZE**2*1d0))
+      sqwol = sqwol + (sum(wolffspin)/(SIZE**2*1d0))**2
+      qwol = qwol + (sum(wolffspin)/(SIZE**2*1d0))**4
+      ewol = ewol + calcenergy(wolffspin, SIZE, temp/100d0)
+      sqewol = sqewol + calcenergy(wolffspin, SIZE, temp/100d0)**2
+      magsupwol = magsupwol + Nswc**2/(SIZE**2*1d0)
     end do
 
     wolfftotal = 100d0 * wolfftotal / TIMEFINAL
+    sqwol = 100d0 * sqwol / TIMEFINAL
+    qwol = 100d0 * qwol / TIMEFINAL
+    ewol = 100d0 * ewol / TIMEFINAL
+    sqewol = 100d0 * sqewol / TIMEFINAL
+    magsupwol = 100d0 * magsupwol / TIMEFINAL
     
 ! Re-initialize Swendswon-Wang lattice. All values in the lattice must be 1
     swenwangspin(:,:) = 1
     swenwangtotal = 0
+    sqsw = 0
+    qsw = 0
+    esw = 0
+    sqesw = 0
+    magsupsw = 0
     do time = 1, TIMEFINAL, 100
-      call swenwang(swenwangspin, SIZE, temp/100d0)
+      call swenwang(swenwangspin, SIZE, temp/100d0, Nsum)
       swenwangtotal = swenwangtotal + abs(sum(swenwangspin)/(SIZE**2*1d0))
+      sqsw = sqsw + (sum(swenwangspin)/(SIZE**2*1d0))**2
+      qsw = qsw + (sum(swenwangspin)/(SIZE**2*1d0))**4
+      esw = esw + calcenergy(swenwangspin, SIZE, temp/100d0)
+      sqesw = sqesw + calcenergy(swenwangspin, SIZE, temp/100d0)**2
+      magsupsw = magsupsw + Nsum
     end do
 
     swenwangtotal = 100d0 * swenwangtotal / TIMEFINAL
+    sqsw = 100d0 * sqsw / TIMEFINAL
+    qsw = 100d0 * qsw / TIMEFINAL
+    esw = 100d0 * esw / TIMEFINAL
+    sqesw = 100d0 * sqesw / TIMEFINAL
+    magsupsw = 100d0 * magsupsw / (TIMEFINAL * SIZE**2 * 1d0)
 
 ! Re-initialize the Metropolis lattice. All values in the lattice must be 1
     spin(:,:) = 1
     spintotal = 0       
+    sqspin = 0
+    qspin = 0
+    etot = 0
+    sqetot = 0
+    absspin = 0
 ! Only 5 options for the exponent for metropolis so calculate them once
     weight = [exp(-800d0/temp),exp(-400d0/temp),1d0,exp(400d0/temp),exp(800d0/temp)]
     do time = 0,TIMEFINAL
       call metropolis(spin, SIZE, weight)
       spintotal = spintotal + abs(sum(spin)/(SIZE**2*1d0))
+      sqspin = sqspin + (sum(spin)/(SIZE**2*1d0))**2
+      qspin = qspin + (sum(spin)/(SIZE**2*1d0))**4
+      etot = etot + calcenergy(spin, SIZE, temp/100d0)
+      sqetot = sqetot + calcenergy(spin, SIZE, temp/100d0)**2
+      absspin = absspin + sum(abs(spin))
 ! We want time to print only once (choose an arbitrary temperature)
       if (temp == 250) then
         WRITE(16,*) sum(spin)/(SIZE**2*1d0), time
       end if
     end do
-    spintotal = spintotal / TIMEFINAL
+    spintotal = 1d0 * spintotal / TIMEFINAL
+    sqspin = 1d0 * sqspin / TIMEFINAL
+    qspin = 1d0 * qspin / TIMEFINAL
+    etot = 1d0 * etot / TIMEFINAL
+    sqetot = 1d0 * sqetot / TIMEFINAL
+    absspin = 1d0 * absspin / TIMEFINAL
       
     call plot_spin(wolffspin, SIZE, temp/100d0)
-    WRITE(15,*) spintotal, temp/100d0
-    WRITE(17,*) swenwangtotal, temp/100d0
-    WRITE(18,*) wolfftotal, temp/100d0
+    WRITE(15,*) spintotal, 1-qspin/(3*sqspin**2), (sqetot - etot**2)/(SIZE**2*1d0),sqspin - absspin**2, temp/100d0
+    WRITE(17,*) swenwangtotal, 1-qsw/(3*sqsw**2), (sqesw - esw**2)/(SIZE**2*1d0), magsupsw, temp/100d0
+    WRITE(18,*) wolfftotal, 1-qwol/(3*sqwol**2), (sqewol - ewol**2)/(SIZE**2*1d0), magsupwol, temp/100d0
   end do
 !!! Close text files !!!                                                                                                                                                 
   call closetextfiles
@@ -128,22 +173,22 @@ contains
 
 subroutine opentextfiles
     integer :: OPEN_STATUS
-    OPEN(UNIT=15,FILE="metrop_mag_temp.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
+    OPEN(UNIT=15,FILE="metrop_temp.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
     if (OPEN_STATUS /= 0) then
-       STOP "------------Error, metrop_mag_temp file not opened properly------------"
+       STOP "------------Error, metrop_temp file not opened properly------------"
     endif
     OPEN(UNIT=16,FILE="metrop_mag_time.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
     if (OPEN_STATUS /= 0) then
        STOP "------------Error, metrop_mag_time file not opened properly------------"
     endif
 
-    OPEN(UNIT=17,FILE="swenwang_mag_temp.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
+    OPEN(UNIT=17,FILE="swenwang_temp.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
     if (OPEN_STATUS /= 0) then
-       STOP "------------Error, swenwang_mag_temp file not opened properly------------"
+       STOP "------------Error, swenwang_temp file not opened properly------------"
     endif
-    OPEN(UNIT=18,FILE="wolff_mag_temp.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
+    OPEN(UNIT=18,FILE="wolff_temp.txt",STATUS="REPLACE",IOSTAT=OPEN_STATUS)
     if (OPEN_STATUS /= 0) then
-       STOP "------------Error, wolff_mag_temp file not opened properly------------"
+       STOP "------------Error, wolff_temp file not opened properly------------"
     endif
 end subroutine
 
--- code/model.f90
+++ code/model.f90
@@ -2,7 +2,7 @@ module model
   implicit none
   
   private growcluster, tryadd, backtrack
-  public metropolis, swenwang, wolff
+  public metropolis, swenwang, wolff, calcenergy
 
 contains
 
@@ -42,14 +42,14 @@ contains
     endif
   end subroutine
 
-  subroutine swenwang(spin, SIZE, temp)
+  subroutine swenwang(spin, SIZE, temp, Nsum)
     !! Passed parameters, intent(in) parameters cannot be altered
     integer,intent(in) :: SIZE
     real(8),intent(in) :: temp
-    integer,intent(inout) :: spin(0:SIZE-1,0:SIZE-1)
+    integer,intent(inout) :: spin(0:SIZE-1,0:SIZE-1), Nsum
 
     !! Subroutine variable declerations
-    integer :: ix, iy, newspin
+    integer :: ix, iy, newspin, Nclus
     real(8) :: r1(0:SIZE-1,0:SIZE-1), r2(0:SIZE-1,0:SIZE-1), r3
     logical :: bonds(0:SIZE-1,0:SIZE-1,2), mark(0:SIZE-1,0:SIZE-1)
 
@@ -60,38 +60,42 @@ contains
     bonds(:,:,2) = (r2 < (1 - exp(-2/temp))*spin*cshift(spin,shift=1,dim=1))
 
     mark = .FALSE.
+    Nsum = 0
 
     do ix = 0, SIZE - 1
       do iy = 0, SIZE - 1
+        Nclus = 0
         call random_number(r3)
         newspin = (2 * nint(r3) - 1) * spin(ix, iy)
-        call backtrack(spin, SIZE, ix, iy, bonds, mark, newspin)
+        call backtrack(spin, SIZE, ix, iy, bonds, mark, newspin, Nclus)
+        Nsum = Nsum + Nclus**2
       enddo
     enddo
   end subroutine
 
-  recursive subroutine backtrack(spin, SIZE, ix, iy, bonds, mark, newspin)
+  recursive subroutine backtrack(spin, SIZE, ix, iy, bonds, mark, newspin, Nclus)
     !! Passed parameters, intent(in) parameters cannot be altered
     integer,intent(in) :: ix, iy, SIZE, newspin
     logical,intent(in) :: bonds(0:SIZE-1, 0:SIZE-1, 2)
-    integer,intent(inout) :: spin(0:SIZE-1, 0:SIZE-1)
+    integer,intent(inout) :: spin(0:SIZE-1, 0:SIZE-1), Nclus
     logical,intent(inout) :: mark(0:SIZE-1, 0:SIZE-1)
 
     if (.NOT. mark(ix, iy)) then
       mark(ix, iy) = .TRUE.
       spin(ix, iy) = newspin
-      if (bonds(ix, iy, 1)) call backtrack(spin, SIZE, modulo(ix + 1, SIZE), iy, bonds, mark, newspin)
-      if (bonds(ix, iy, 2)) call backtrack(spin ,SIZE, ix, modulo(iy - 1, SIZE), bonds, mark, newspin)
-      if (bonds(modulo(ix - 1, SIZE), iy, 1)) call backtrack(spin, SIZE, modulo(ix - 1, SIZE), iy, bonds, mark, newspin)
-      if (bonds(ix, modulo(iy + 1, SIZE), 2)) call backtrack(spin, SIZE, ix, modulo(iy + 1, SIZE), bonds, mark, newspin)
+      Nclus = Nclus + 1
+      if (bonds(ix, iy, 1)) call backtrack(spin, SIZE, modulo(ix + 1, SIZE), iy, bonds, mark, newspin, Nclus)
+      if (bonds(ix, iy, 2)) call backtrack(spin ,SIZE, ix, modulo(iy - 1, SIZE), bonds, mark, newspin, Nclus)
+      if (bonds(modulo(ix - 1, SIZE), iy, 1)) call backtrack(spin, SIZE, modulo(ix - 1, SIZE), iy, bonds, mark, newspin, Nclus)
+      if (bonds(ix, modulo(iy + 1, SIZE), 2)) call backtrack(spin, SIZE, ix, modulo(iy + 1, SIZE), bonds, mark, newspin, Nclus)
     endif
   end subroutine
 
-  subroutine wolff(spin, SIZE, temp)
+  subroutine wolff(spin, SIZE, temp, Nswc)
     !! Passed parameters, intent(in) parameters cannot be altered
     integer,intent(in) :: SIZE
     real(8),intent(in) :: temp
-    integer,intent(inout) :: spin(0:SIZE-1,0:SIZE-1)
+    integer,intent(inout) :: spin(0:SIZE-1,0:SIZE-1), Nswc
 
     !! Subroutine variable declerations
     integer :: ix, iy
@@ -106,29 +110,31 @@ contains
 
     mark = .FALSE.
 
-    call growcluster(spin, SIZE, ix, iy, temp, mark, -spin(ix, iy))
+    Nswc = 0
+    call growcluster(spin, SIZE, ix, iy, temp, mark, -spin(ix, iy), Nswc)
   end subroutine
 
-  subroutine growcluster(spin, SIZE, ix, iy, temp, mark, newspin)
+  subroutine growcluster(spin, SIZE, ix, iy, temp, mark, newspin, Nswc)
     !! Passed parameters, intent(in) parameters cannot be altered
     integer,intent(in) :: SIZE, newspin
     real(8),intent(in) :: temp
-    integer :: spin(0:SIZE-1, 0:SIZE-1), ix, iy
+    integer :: spin(0:SIZE-1, 0:SIZE-1), ix, iy, Nswc
     logical,intent(inout) :: mark(0:SIZE-1, 0:SIZE-1)
 
+    Nswc = Nswc + 1
     spin(ix, iy) = newspin
     mark(ix, iy) = .TRUE.
-    if (.NOT.(mark(modulo(ix + 1, SIZE), iy))) call tryadd(spin, SIZE, modulo(ix + 1, SIZE), iy, temp, mark, newspin)
-    if (.NOT.(mark(modulo(ix - 1, SIZE), iy))) call tryadd(spin, SIZE, modulo(ix - 1, SIZE), iy, temp, mark, newspin)
-    if (.NOT.(mark(ix, modulo(iy + 1, SIZE)))) call tryadd(spin, SIZE, ix, modulo(iy + 1, SIZE), temp, mark, newspin)
-    if (.NOT.(mark(ix, modulo(iy - 1, SIZE)))) call tryadd(spin, SIZE, ix, modulo(iy - 1, SIZE), temp, mark, newspin)
+    if (.NOT.(mark(modulo(ix + 1, SIZE), iy))) call tryadd(spin, SIZE, modulo(ix + 1, SIZE), iy, temp, mark, newspin, Nswc)
+    if (.NOT.(mark(modulo(ix - 1, SIZE), iy))) call tryadd(spin, SIZE, modulo(ix - 1, SIZE), iy, temp, mark, newspin, Nswc)
+    if (.NOT.(mark(ix, modulo(iy + 1, SIZE)))) call tryadd(spin, SIZE, ix, modulo(iy + 1, SIZE), temp, mark, newspin, Nswc)
+    if (.NOT.(mark(ix, modulo(iy - 1, SIZE)))) call tryadd(spin, SIZE, ix, modulo(iy - 1, SIZE), temp, mark, newspin, Nswc)
   end subroutine
 
-  subroutine tryadd(spin, SIZE, ix, iy, temp, mark, newspin)
-    !! Passed parameters, intent(in) paramters cannot be altered
+  subroutine tryadd(spin, SIZE, ix, iy, temp, mark, newspin, Nswc)
+    !! Passed parameters, intent(in) parameters cannot be altered
     integer,intent(in) :: SIZE, newspin
     real(8),intent(in) :: temp
-    integer :: spin(0:SIZE-1, 0:SIZE-1), ix, iy
+    integer :: spin(0:SIZE-1, 0:SIZE-1), ix, iy, Nswc
     logical,intent(inout) :: mark(0:SIZE-1, 0:SIZE-1)
 
     !! Subroutine variable declerations
@@ -137,8 +143,30 @@ contains
     if (spin(ix, iy) /= newspin) then
       call random_number(r1)
       if (r1 < (1 - exp(-2 / temp))) then
-        call growcluster(spin, SIZE, ix, iy, temp, mark, newspin)
+        call growcluster(spin, SIZE, ix, iy, temp, mark, newspin, Nswc)
       end if
     end if
   end subroutine
+
+  real(8) function calcenergy(spin, SIZE, temp) result(ener)
+    !! Passed paramterers, intent(in) parameters cannot be altered
+    integer,intent(in) :: SIZE, spin(0:SIZE-1, 0:SIZE-1)
+    real(8),intent(in) :: temp
+
+    !! Subroutine variable declerations
+    integer :: ix, iy
+    real(8) :: J
+
+    J = 1/temp
+
+    ener = 0
+    do ix = 0, SIZE - 1
+      do iy = 0, SIZE - 1
+        ener = ener - J * spin(ix, iy) * spin(modulo(ix - 1, SIZE), iy)
+        ener = ener - J * spin(ix, iy) * spin(modulo(ix + 1, SIZE), iy)
+        ener = ener - J * spin(ix, iy) * spin(ix, modulo(iy - 1, SIZE))
+        ener = ener - J * spin(ix, iy) * spin(ix, modulo(iy + 1, SIZE))
+      end do
+    end do
+  end function
 end module
