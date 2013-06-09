6f7156d4ad44a1ad330bb22371a558fdfb028c1d
Chiel Donkers <mmhdonkers@vvtp.tudelft.nl>
2013-06-09 22:25:07 +0200

    last code update

--- code/ising.f90
+++ code/ising.f90
@@ -44,14 +44,14 @@ program ising
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !! INPUT: Row and column size
-  integer,parameter :: SIZE = 32
+  integer,parameter :: SIZE = 16
 
 !! INPUT: Final temperature (Kelvin x 100), final time, stepsizeloops
   integer,parameter :: TEMPFINAL = 350, TIMEFINAL = 100000
 
 !! fortran begins indexing from 1. Start it from 0 because the rand() starts from 0
   integer :: spin(0:SIZE-1,0:SIZE-1), temp, time
-  real(8) :: weight(-2:2), spintotal, sqspin, qspin, etot, sqetot, absspin
+  real(8) :: weight(-2:2), spintotal, sqspin, qspin, etot, sqetot
   integer :: wolffspin(0:SIZE-1, 0:SIZE-1), Nswc
   real(8) :: wolfftotal, sqwol, qwol, ewol, sqewol, magsupwol
   integer :: swenwangspin(0:SIZE-1, 0:SIZE-1), Nsum
@@ -91,7 +91,7 @@ program ising
       qwol = qwol + (sum(wolffspin)/(SIZE**2*1d0))**4
       ewol = ewol + calcenergy(wolffspin, SIZE, temp/100d0)
       sqewol = sqewol + calcenergy(wolffspin, SIZE, temp/100d0)**2
-      magsupwol = magsupwol + Nswc**2/(SIZE**2*1d0)
+      magsupwol = magsupwol + Nswc**2
     end do
 
     wolfftotal = 100d0 * wolfftotal / TIMEFINAL
@@ -100,6 +100,7 @@ program ising
     ewol = 100d0 * ewol / TIMEFINAL
     sqewol = 100d0 * sqewol / TIMEFINAL
     magsupwol = 100d0 * magsupwol / TIMEFINAL
+    magsupwol = magsupwol / (SIZE**2 * 1d0)
     
 ! Re-initialize Swendswon-Wang lattice. All values in the lattice must be 1
     swenwangspin(:,:) = 1
@@ -124,7 +125,8 @@ program ising
     qsw = 100d0 * qsw / TIMEFINAL
     esw = 100d0 * esw / TIMEFINAL
     sqesw = 100d0 * sqesw / TIMEFINAL
-    magsupsw = 100d0 * magsupsw / (TIMEFINAL * SIZE**2 * 1d0)
+    magsupsw = 100d0 * magsupsw / TIMEFINAL
+    magsupsw = magsupsw / (SIZE**4 * 1d0)
 
 ! Re-initialize the Metropolis lattice. All values in the lattice must be 1
     spin(:,:) = 1
@@ -133,7 +135,6 @@ program ising
     qspin = 0
     etot = 0
     sqetot = 0
-    absspin = 0
 ! Only 5 options for the exponent for metropolis so calculate them once
     weight = [exp(-800d0/temp),exp(-400d0/temp),1d0,exp(400d0/temp),exp(800d0/temp)]
     do time = 0,TIMEFINAL
@@ -143,7 +144,6 @@ program ising
       qspin = qspin + (sum(spin)/(SIZE**2*1d0))**4
       etot = etot + calcenergy(spin, SIZE, temp/100d0)
       sqetot = sqetot + calcenergy(spin, SIZE, temp/100d0)**2
-      absspin = absspin + sum(abs(spin))
 ! We want time to print only once (choose an arbitrary temperature)
       if (temp == 250) then
         WRITE(16,*) sum(spin)/(SIZE**2*1d0), time
@@ -154,10 +154,9 @@ program ising
     qspin = 1d0 * qspin / TIMEFINAL
     etot = 1d0 * etot / TIMEFINAL
     sqetot = 1d0 * sqetot / TIMEFINAL
-    absspin = 1d0 * absspin / TIMEFINAL
       
     call plot_spin(wolffspin, SIZE, temp/100d0)
-    WRITE(15,*) spintotal, 1-qspin/(3*sqspin**2), (sqetot - etot**2)/(SIZE**2*1d0),sqspin - absspin**2, temp/100d0
+    WRITE(15,*) spintotal, 1-qspin/(3*sqspin**2), (sqetot - etot**2)/(SIZE**2*1d0), (sqspin - spintotal**2), temp/100d0
     WRITE(17,*) swenwangtotal, 1-qsw/(3*sqsw**2), (sqesw - esw**2)/(SIZE**2*1d0), magsupsw, temp/100d0
     WRITE(18,*) wolfftotal, 1-qwol/(3*sqwol**2), (sqewol - ewol**2)/(SIZE**2*1d0), magsupwol, temp/100d0
   end do
